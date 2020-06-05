bc5317e497e536dfe48202d828a030add887d9f4
NikitaPnch <nikitapnch@gmail.com>
2020-06-06 00:29:35 +0500

    - fix

--- app/src/main/java/com/example/newsapp/api/requests/News.kt
+++ app/src/main/java/com/example/newsapp/api/requests/News.kt
@@ -1,7 +1,7 @@
 package com.example.newsapp.api.requests
 
-import com.example.newsapp.api.model.APINews
 import com.example.newsapp.api.API
+import com.example.newsapp.api.model.APINews
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,8 +11,6 @@ interface News {
     @GET("v2/top-headlines")
     fun getTopHeadlines(
         @Query("country") country: String? = null,
-        @Query("category") category: String? = null,
-        @Query("q") query: String? = null,
         @Query("pageSize") pageSize: Int? = API.PAGE_SIZE
     ): Single<APINews>
 }
