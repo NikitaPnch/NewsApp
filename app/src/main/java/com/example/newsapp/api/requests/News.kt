406e50fbe57f0a37a8e2128ed0c8cfb0ec2bdd8b
NikitaPnch <nikitapnch@gmail.com>
2020-06-22 21:07:59 +0500

    - добавил комментарии

--- app/src/main/java/com/example/newsapp/api/requests/News.kt
+++ app/src/main/java/com/example/newsapp/api/requests/News.kt
@@ -8,12 +8,24 @@ import retrofit2.http.Query
 
 interface News {
 
+    /** Получает главные заголовки за последнее время
+     * @param country передать страну в формате двух букв по стандарту ISO 3166-1
+     * @param pageSize кол-во результатов на страницу, мин = 20, макс = 100
+     */
     @GET("v2/top-headlines")
     fun getTopHeadlines(
         @Query("country") country: String? = null,
         @Query("pageSize") pageSize: Int? = API.PAGE_SIZE
     ): Single<APINews>
 
+    /** Поиск по новостям
+     * @param q строка запроса
+     * @param sortBy фильтр результатов
+     * @param from поиск от конкретной даты
+     * @param to поиск по конкретную даты
+     * @param language на каком языке нужно найти новости, две буквы по стандарту ISO-639-1
+     * @param pageSize кол-во результатов на страницу, мин = 20, макс = 100
+     */
     @GET("v2/everything")
     fun searchEverything(
         @Query("q") query: String,
