64d6cb14db642207151e49369dc1f72d1e48f1f5
NikitaPnch <nikitapnch@gmail.com>
2020-06-25 17:29:55 +0500

    - увеличил размер кеша у картинок

--- app/src/main/java/com/example/newsapp/App.kt
+++ app/src/main/java/com/example/newsapp/App.kt
@@ -2,6 +2,7 @@ package com.example.newsapp
 
 import android.app.Application
 import com.example.newsapp.extensions.appModule
+import com.example.newsapp.extensions.getImagePipelineConfig
 import com.facebook.drawee.backends.pipeline.Fresco
 import org.koin.core.context.startKoin
 import timber.log.Timber
@@ -10,7 +11,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Fresco.initialize(this)
+        Fresco.initialize(this, getImagePipelineConfig(this))
         Timber.plant(Timber.DebugTree())
         startKoin {
             modules(appModule)
--- app/src/main/java/com/example/newsapp/extensions/FrescoUtils.kt
+++ app/src/main/java/com/example/newsapp/extensions/FrescoUtils.kt
@@ -1,13 +1,18 @@
 package com.example.newsapp.extensions
 
+import android.content.Context
 import android.net.Uri
+import com.facebook.cache.disk.DiskCacheConfig
+import com.facebook.common.util.ByteConstants
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.drawee.generic.RoundingParams
 import com.facebook.drawee.view.SimpleDraweeView
 import com.facebook.imagepipeline.common.Priority
 import com.facebook.imagepipeline.common.ResizeOptions
+import com.facebook.imagepipeline.core.ImagePipelineConfig
 import com.facebook.imagepipeline.request.ImageRequest
 import com.facebook.imagepipeline.request.ImageRequestBuilder
+
 
 fun SimpleDraweeView.createImageRequest(uri: String) {
     val request = ImageRequestBuilder.newBuilderWithSource(Uri.parse(uri))
@@ -25,5 +30,21 @@ fun SimpleDraweeView.createImageRequest(uri: String) {
     this.controller = Fresco.newDraweeControllerBuilder()
         .setOldController(this.controller)
         .setImageRequest(request)
+        .build()
+}
+
+fun getDefaultMainDiskCacheConfig(context: Context): DiskCacheConfig? {
+    return DiskCacheConfig.newBuilder(context)
+        .setBaseDirectoryPathSupplier { context.applicationContext.cacheDir }
+        .setBaseDirectoryName("image_cache")
+        .setMaxCacheSize(80 * ByteConstants.MB.toLong())
+        .setMaxCacheSizeOnLowDiskSpace(10 * ByteConstants.MB.toLong())
+        .setMaxCacheSizeOnVeryLowDiskSpace(2 * ByteConstants.MB.toLong())
+        .build()
+}
+
+fun getImagePipelineConfig(context: Context): ImagePipelineConfig? {
+    return ImagePipelineConfig.newBuilder(context)
+        .setMainDiskCacheConfig(getDefaultMainDiskCacheConfig(context))
         .build()
 }
